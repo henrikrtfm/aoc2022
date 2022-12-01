4b6956b92cda9feed9e09e5d6939ca7ff9e6a881
Henrik Edsparr <henrik.edsparr@gmail.com>
2022-12-01 16:17:53 +0100

    Refactored

--- src/day01/Day01.kt
+++ src/day01/Day01.kt
@@ -1,6 +1,7 @@
 package day01
 
-import utils.Resources.resourceAsListOfString
+//import utils.Resources.resourceAsListOfString
+import utils.Resources.resourceAsText
 
 fun main() {
     fun part1(input: List<Int>): Int {
@@ -11,15 +12,20 @@ fun main() {
         return input.sortedDescending().take(3).sum()
     }
 
-    val input = resourceAsListOfString("src/day01/Day01.txt")
-    val indexes = input.foldIndexed(listOf(0)){ index, acc, s -> if(s == "") acc+(index) else acc} + input.size
-    var caloriesList = mutableListOf<List<Int>>()
-    indexes.zipWithNext().forEach {
-        caloriesList.add(input.subList(it.first,it.second)
-            .filter { s: String -> s != "" }
-            .map { it2 -> it2.toInt() })
-    }
-    val caloriesSummed = caloriesList.map{ it.sum() }
+    val input = resourceAsText("src/day01/Day01.txt").split("\n\n").map { it.split("\n") }
+    val caloriesSummed = input.map { it.sumOf(String::toInt) }
     println(part1(caloriesSummed))
     println(part2(caloriesSummed))
+
+
+    //Solution if the file has CRLF
+    /*    val input = resourceAsListOfString("src/day01/Day01.txt")
+        val indexes = input.foldIndexed(listOf(0)){ index, acc, s -> if(s == "") acc+(index) else acc} + input.size
+        var caloriesList = mutableListOf<List<Int>>()
+        indexes.zipWithNext().forEach {
+            caloriesList.add(input.subList(it.first,it.second)
+                .filter { s: String -> s != "" }
+                .map { it2 -> it2.toInt() })
+        }
+        val caloriesSummed = caloriesList.map{ it.sum() }*/
 }
