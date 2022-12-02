bab684724b653bde3febbdcbdcf5a76048baf385
Henrik Edsparr <henrik.edsparr@gmail.com>
2022-12-02 09:32:27 +0100

    Refactored

--- src/day01/Day01.kt
+++ src/day01/Day01.kt
@@ -1,6 +1,5 @@
 package day01
 
-//import utils.Resources.resourceAsListOfString
 import utils.Resources.resourceAsText
 
 fun main() {
@@ -16,16 +15,4 @@ fun main() {
     val caloriesSummed = input.map { it.sumOf(String::toInt) }
     println(part1(caloriesSummed))
     println(part2(caloriesSummed))
-
-
-    //Solution if the file has CRLF
-    /*    val input = resourceAsListOfString("src/day01/Day01.txt")
-        val indexes = input.foldIndexed(listOf(0)){ index, acc, s -> if(s == "") acc+(index) else acc} + input.size
-        var caloriesList = mutableListOf<List<Int>>()
-        indexes.zipWithNext().forEach {
-            caloriesList.add(input.subList(it.first,it.second)
-                .filter { s: String -> s != "" }
-                .map { it2 -> it2.toInt() })
-        }
-        val caloriesSummed = caloriesList.map{ it.sum() }*/
 }
