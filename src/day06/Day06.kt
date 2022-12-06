f4fc21b082c0fe75740252820cf7e913b5fd4e17
Henrik Edsparr <henrik.edsparr@gmail.com>
2022-12-06 09:52:25 +0100

    Solution to Day 6 of Advent Of Code 2022

---
+++ src/day06/Day06.kt
@@ -0,0 +1,25 @@
+package day06
+
+import utils.Resources.resourceAsString
+
+fun main(){
+
+    fun findMarker(windowSize: Int, buffer: String): Int{
+        return windowSize + buffer.windowed(windowSize, 1).map { it.allUnique() }.indexOfFirst { it }
+    }
+
+    fun part1(input: String): Int{
+        return findMarker(4,input)
+    }
+
+    fun part2(input: String): Int{
+        return findMarker(14, input)
+    }
+
+    val input = resourceAsString("src/day06/Day06.txt")
+    println(part1(input))
+    println(part2(input))
+
+}
+
+fun String.allUnique(): Boolean = all(hashSetOf<Char>()::add)
